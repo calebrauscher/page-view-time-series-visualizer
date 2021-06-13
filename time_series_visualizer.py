1acccf3472ff0275fbecc92f08d63fc7b99e86a2
Caleb <cjr214@gmail.com>
2021-06-13 11:00:08 -0400

    Add BoxPlot

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -32,11 +32,11 @@ def draw_bar_plot():
     months = {"01":"January", "02":"February", "03":"March", "04":"April", "05":"May", "06":"June", "07":"July", "08":"August", "09":"September", "10":"October", "11":"November", "12":"December"}
 
     df_bar = df_bar.reset_index()
-    df_bar["month"] = df_bar["date"].apply(lambda row: months[row[5:7]])
-    df_bar["month"] = pd.Categorical(df_bar["month"], categories=months.values())
+    df_bar["Months"] = df_bar["date"].apply(lambda row: months[row[5:7]])
+    df_bar["Months"] = pd.Categorical(df_bar["Months"], categories=months.values())
 
     df_bar["year"] = df_bar["date"].apply(lambda row: row[:4])
-    df_pivot = pd.pivot_table(df_bar, values="value", index="year", columns="month", aggfunc=np.mean)
+    df_pivot = pd.pivot_table(df_bar, values="value", index="year", columns="Months", aggfunc=np.mean)
 
     # Draw bar plot
     ax = df_pivot.plot(kind="bar")
@@ -53,21 +53,30 @@ def draw_box_plot():
     # Prepare data for box plots (this part is done!)
     df_box = df.copy()
     df_box.reset_index(inplace=True)
-    df_box['year'] = [d.year for d in df_box.date]
-    df_box['month'] = [d.strftime('%b') for d in df_box.date]
+
+    months = {"01":"Jan", "02":"Feb", "03":"Mar", "04":"Apr", "05":"May", "06":"Jun", "07":"Jul", "08":"Aug", "09":"Sep", "10":"Oct", "11":"Nov", "12":"Dec"}
+
+    df_box = df_box.rename(columns={"value":"Page Views"})
 
     # Draw box plots (using Seaborn)
+    fig, (ax1, ax2) = plt.subplots(ncols=2)
+ 
+    sns.set(rc={'figure.figsize':(30,17)})
+    sns.set_style("whitegrid")
+    fig.tight_layout()
+    df_box["Year"] = df_box["date"].apply(lambda row: row[:4])
+    df_box = df_box.sort_values(by=["Year"])
+
+    sns.boxplot(x="Year", y="Page Views", data=df_box, ax=ax1).set_title("Year-wise Box Plot (Trend)")
 
 
 
+    df_box["Month"] = df_box["date"].apply(lambda row: row[5:7])
+    df_box = df_box.sort_values(by=["Month"])
+    df_box["Month"] = df_box["Month"].apply(lambda row: months.get(row))
 
+    sns.boxplot(x="Month", y="Page Views", data=df_box, ax=ax2).set_title("Month-wise Box Plot (Seasonality)")
 
     # Save image and return fig (don't change this part)
     fig.savefig('box_plot.png')
     return fig
-
-def get_common_years(df, years):
-  data = []
-  for year in years:
-    data.append([list(df[df["year"] == year]["month"].to_numpy()), list(df[df["year"] == year]["value"].to_numpy())])
-  return data
